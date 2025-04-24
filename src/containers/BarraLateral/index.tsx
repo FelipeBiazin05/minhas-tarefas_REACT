3d30009f5e3e7e625354fb7966679b42cdb55d33
FelipeBiazin05 <142363711+FelipeBiazin05@users.noreply.github.com>
2025-04-23 21:09:39 -0300

    Fazendo a barra lateral do projeto

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,3 +1,20 @@
-const BarraLateral = () => <aside>Barra Lateral</aside>
+import FiltroCard from '../../components/FiltroCard'
+import * as S from './styles'
+
+const BarraLateral = () => (
+  <S.Aside>
+    <div>
+      <S.Campo type="text" placeholder="Procurar" />
+      <S.Filtros>
+        <FiltroCard legenda="Pendentes" contador={1} />
+        <FiltroCard legenda="Concluidas" contador={3} />
+        <FiltroCard legenda="Urgentes" contador={2} />
+        <FiltroCard legenda="Importantes" contador={7} />
+        <FiltroCard legenda="Normal" contador={4} />
+        <FiltroCard legenda="Todas" contador={17} ativo />
+      </S.Filtros>
+    </div>
+  </S.Aside>
+)
 
 export default BarraLateral
